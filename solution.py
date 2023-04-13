5467c6947e694b8fd9060e90254c995ef347625a
1imbooo <130186620+1imbooo@users.noreply.github.com>
2023-04-13 23:57:52 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,15 +1,18 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import mannwhitneyu
+from scipy import stats
 
 
 chat_id = 661128504 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     alpha = 0.09
-    stat, p = mannwhitneyu(x, y, alternative='greater')
+    t_stat, p_value = stats.ttest_ind(y, x, equal_var=False, alternative="greater")
     
-    if p < alpha:
+    # Сравнение с критическим значением
+    t_crit = stats.t.ppf(1-alpha/2, len(x)+len(y)-2)   
+    
+    if t_stat > t_crit and p_value < alpha:
         return True
     else:
         return False
